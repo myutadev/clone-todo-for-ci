53737cd06d89cabc47ae441ad92556d6a8148236
myutadev <myuta.dev@gmail.com>
2024-12-20 18:29:42 +0900

    誤ったところに入っていたcommentを削除

--- database/migrations/2024_12_19_073139_create_tasks_table.php
+++ database/migrations/2024_12_19_073139_create_tasks_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id()->comment('主キー');
-            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete()->comment;
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('name')->comment('タスク名');
             $table->text('description')->nullable()->comment('詳細説明');
             $table->integer('priority')->default(1)->comment('優先度');
